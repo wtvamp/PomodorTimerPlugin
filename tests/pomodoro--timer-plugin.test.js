e4d4dab94c05dd29ccabb9fd0756cecf2c4db0ad
KMART <93807261+KMArtwork@users.noreply.github.com>
2024-06-19 17:06:34 -0700

    update unit tests

--- tests/pomodoro--timer-plugin.test.js
+++ tests/pomodoro--timer-plugin.test.js
@@ -3,6 +3,8 @@ import PomodoroTimerPlugin from '../src/PomodoroTimerPlugin';
 describe('PomodoroTimerPlugin', () => {
   let chart;
   let timerInput;
+  let shortBreakElement;
+  let longBreakElement;
   let startButton;
   let stopButton;
   let resetButton;
@@ -12,6 +14,8 @@ describe('PomodoroTimerPlugin', () => {
     document.body.innerHTML = `
       <canvas id="pomodoroChart"></canvas>
       <input type="text" id="timerInput" value="2" />
+      <input type="text" id="shortBreakInput" value="1" />
+      <input type="text" id="longBreakInput" value="2" />
       <button id="timerStart">Start</button>
       <button id="timerStop">Stop</button>
       <button id="timerReset">Reset</button>
@@ -23,6 +27,8 @@ describe('PomodoroTimerPlugin', () => {
         update: jest.fn() // Mock the chart update method
     };
     timerInput = document.getElementById('timerInput');
+    shortBreakElement = document.getElementById('shortBreakInput');
+    longBreakElement = document.getElementById('longBreakInput');
     startButton = document.getElementById('timerStart');
     stopButton = document.getElementById('timerStop');
     resetButton = document.getElementById('timerReset');
@@ -30,6 +36,8 @@ describe('PomodoroTimerPlugin', () => {
     // Initialize the Pomodoro Timer Plugin
     PomodoroTimerPlugin.install(chart, {}, {
       timerInputId: 'timerInput',
+      shortBreakInputId: 'shortBreakInput',
+      longBreakInputId: 'longBreakInput',
       startButtonId: 'timerStart',
       stopButtonId: 'timerStop',
       resetButtonId: 'timerReset'
@@ -46,7 +54,7 @@ describe('PomodoroTimerPlugin', () => {
     expect(resetButton.disabled).toBe(false); // Reset button should be enabled
 
     // Simulate timer update
-    jest.advanceTimersByTime(1000); // Advance timer by 1 second
+    jest.advanceTimersByTime(2000); // Advance timer by 1 second; Set to 2s to account for 'timeLeft' property being set at -1 on install.
     expect(chart.data.datasets[0].data[0]).toBe(1); // Check if chart data is updated
     // Add more assertions as needed
   });
